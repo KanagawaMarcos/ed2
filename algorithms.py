a8a1f097891dd26a2f861fc91fd958eef8b71306
Marcos Costa <mcs1901@outlook.com>
2018-04-07 12:30:13 -0300

    create the function overview to convert the text to array

--- algorithms.py
+++ algorithms.py
@@ -3,16 +3,19 @@ import os
 class InsertSort():
 
 	def sort(self ,filename ):
-		with open(filename) as f:
-			for line in f:
-				for word in line:
-					print(word)
+		pass
 
 
 class SelectionSort():
 
-	def sort(self, filename):
+	def sort(self, array):
 		pass
 
 	def this_comes_first_than_that(self, this, that):
 		pass
+
+def convert_file_to_array(filename):
+	with open(filename) as f:
+			for line in f:
+				for word in line:
+					print(word)
