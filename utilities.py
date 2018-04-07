bdaa7c9ff581a4169e981526ce69063807c31aa5
Marcos Costa <mcs1901@outlook.com>
2018-04-07 17:06:27 -0300

    create convert_file_to_dictionary()

--- utilities.py
+++ utilities.py
@@ -1,5 +1,14 @@
 import os
 
+def convert_file_to_array(filename):
+	array = []
+
+	with open(filename) as file:
+		for line in file:
+			array += line.split()
+
+	return array
+
 def convert_file_to_dictionary(filename):
 	array = []
 
