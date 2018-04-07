ee0d54fa6d29d57436f1689328fe5179ed8dc734
Marcos Costa <mcs1901@outlook.com>
2018-04-07 17:51:59 -0300

    Insertion Sort Updated

---
+++ InsertionSort.py
@@ -0,0 +1,34 @@
+import utilities
+
+class InsertionSort():
+
+	# Sort an array using Selection Sort
+	def sort_array(self, array):
+		
+		# Loop through the entire array
+		for current_foward in range( 1, len(array) ):
+			
+			# Go on each element before the current
+
+			current_backwards = current_foward
+			# Check which word comes first in alphabetic order
+
+			this = array[current_backwards-1]
+			that = array[current_backwards]
+
+			while current_backwards > 0 and utilities.this_word_comes_first_than_that(this,that):
+					# Swap the words
+
+					i = current_backwards
+					j = (current_backwards - 1)
+
+					array[i],array[j] = array[j],array[i]
+
+					current_backwards -= 1
+
+					this = array[current_backwards-1]
+					that = array[current_backwards]
+
+			
+
+
