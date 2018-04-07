6d732226e3e54d5a34cbfdb8cd3c1017123b12fa
Marcos Costa <mcs1901@outlook.com>
2018-04-07 13:36:13 -0300

    update the (this comes frist than that) function

--- algorithms.py
+++ algorithms.py
@@ -11,12 +11,32 @@ class SelectionSort():
 	def sort(self, array):
 		pass
 
-	def __this_comes_first_than_that(self, this, that):
-		pass
+	def __this_comes_first_than_that(self, this, that, minimum_word_size=4):
+
+		# If both words has the minimum amout of letters or more
+
+		if(len(this) >= minimum_word_size and len(that) >= minimum_word_size):
+
+			# Which one has the greater size?
+			
+			greater_size = len(this)
+			if len(that) > greater_size:
+				greater_size = len(that)
+
+			# Return the True if "this" comes first, otherwise return False
+			
+			for current_letter in range(greater_size):
+
+				if this[current_letter].lower() > that[current_letter].lower():
+					return True
+				elif this[current_letter].lower() < that[current_letter].lower():
+					return False
 
 def convert_file_to_array(filename):
 	array = []
+
 	with open(filename) as f:
 			for line in f:
 				array += line.split()
-			
+
+	return array
--- ordenar.py
+++ ordenar.py
@@ -2,9 +2,11 @@ import sys
 import algorithms
 
 filename = '1 primeira entrada.txt' 
+filename = 'teste.txt' 
 
 sorting_algorithm_choosen = algorithms.InsertSort()
 sorting_algorithm_choosen.sort(filename)
 
 
-algorithms.convert_file_to_array(filename)
+#array = algorithms.convert_file_to_array(filename)
+#print(array)
