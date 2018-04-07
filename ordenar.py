9c61ebf5e50620c80ce963eb79af1c2c830a1841
Marcos Costa <mcs1901@outlook.com>
2018-04-07 12:32:27 -0300

    update the main file

--- ordenar.py
+++ ordenar.py
@@ -1,7 +1,10 @@
 import sys
 import algorithms
 
-
+filename = '2 segunda entrada.txt' 
 
 sorting_algorithm_choosen = algorithms.InsertSort()
-sorting_algorithm_choosen.sort('2 segunda entrada.txt')
+sorting_algorithm_choosen.sort(filename)
+
+
+algorithms.convert_file_to_array(filename)
