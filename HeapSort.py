bfa0754172405ba274a9a66db08df5a9807c25e8
Marcos Costa <mcs1901@outlook.com>
2018-04-07 21:06:55 -0300

    HeapSort created

---
+++ HeapSort.py
@@ -0,0 +1,42 @@
+import utilities
+
+class HeapSort():
+
+	def sort_array(self, array):
+		
+		# Build the Max Heap (Every child must be lesser than it's parent)
+		self.build_max_heap(array)
+		
+		# Goes from the (last-1) sorted element to the first element of the array
+		for i in range(len(array),-1,-1):
+			
+			# Swap the first element and the last
+			array[0],array[-1] = array[-1],array[0]
+
+			# Assume that part of the tree is already sorted, and and sort only one node
+			self.heapfy(array, i)
+
+	def build_max_heap(self, array):
+		for i in range( int(len(array)) , -1 , -1):
+			self.heapfy(array, i)
+
+	def heapfy(self, array, i):
+		largest = i
+		left_index = 2*i
+		right_index = 2*i+1
+		
+		# See if left child of root exists and is greater than it's parent
+		if left_index < len(array) and utilities.this_word_comes_first_than_that(array[i],array[left_index]) :
+		    largest = left_index
+
+		# See if right child of root exists and is greater than it's parent
+		if right_index < len(array) and utilities.this_word_comes_first_than_that(array[largest],array[right_index]) :
+		    largest = right_index
+
+		# Change root, if needed
+		if largest != i:
+			# Swap
+		    array[i],array[largest] = array[largest],array[i]  
+
+		    # Heapify the parent, until it's no longer required.
+		    self.heapfy(array, largest)
--- ordenar.py
+++ ordenar.py
@@ -1,18 +1,23 @@
 import utilities
 from SelectionSort import SelectionSort
 from InsertionSort import InsertionSort
+from HeapSort import HeapSort
+
 
 
 
 filename = '1 primeira entrada.txt' 
-filename = 'deleteme.teste.txt' 
+#filename = 'deleteme.teste.txt' 
 
-#sorting_algorithm_choosen = SelectionSort()
+sorting_algorithm_choosen = SelectionSort()
 sorting_algorithm_choosen = InsertionSort()
+sorting_algorithm_choosen = HeapSort()
+
+
 
 
 array = utilities.convert_file_to_array(filename)
-print(array)
+#print(array)
 
 sorting_algorithm_choosen.sort_array(array)
 
