34d3f9935e3493bf6b78fb3b3efad7c67ec33b37
Victor Rogerio <38105628+VictoRinor@users.noreply.github.com>
2018-04-20 07:22:20 -0300

    Compilando e Ordenando
    Então, funciona, o unico problema são as letras maiusculas e minusculas, pois n estou conseguindo usar a função this_word_comes_first_than_that

--- Quicksort.py
+++ Quicksort.py
@@ -1,81 +1,79 @@
-# Estou usando array, mas, é possível alterar pra hash 
+#This Python file uses the following encoding: utf-8 
 import utilities
 
-class Quick(object):
-	
+class Quick():
 
-	def particiona(self, array, begin, end):
-				i=begin
-				j=end
-				meio = (i+j)/2
-				pivo=array[meio]
+        def sort_array(self,array):
+                        end=len(array)
+                        self.QuickSort(array,0,end)
+                        return array
 
-				#Troca o pivô com o primeiro elemento
-				array[i],array[meio] = array[meio],array[i]
-				i=+1
+                        
+        def particiona(self, array, begin, end):
+                        i=begin
+                        j=end-1
+                        meio = (i+j)/2
+                        pivo=array[meio]
 
-				while i<j
-
-							loop_j =0
-
-							#Atribui a variavél left a palavra em array[i]
-							left=array[i]
-						
-							
-							#Testa se a palavra em left vem antes da palavra em pivô
-							if self.__this_word_comes_first_than_that(left, pivo):
-							
-									pivo=pivo
-									i=+1
-
-							else:
-
-								while not lopp_j:
-
-										#Atribui a variavél right a palavra em array[j]
-										right=array[j]
-
-										#Testa se a palavra no pivô vem antes da palavra em right
-										if self.__this_word_comes_first_than_that(pivo, right):
-
-												j=-1
-
-										else:
-
-											#Troca as palavras no array nas posições i e j
-											array[i],array[j] = array[j],array[i]
-											
-											i+1
-											j+1
-
-											#Encerra o Loop
-											loop_j=1
-
-				if i>j:
-
-					#Troca as palavras no array nas posições meio e j
-					array[meio],array[j]=array[j],array[meio]
-
-					return meio
+                        #Troca o pivÃ´ com o primeiro elemento
+                        array[i],array[meio] = array[meio],array[i]
+                        i=+1
+                        loop_j=0
 
 
-	def quicksort(self, array, begin, end):
-			if end =1
-					return array
+                        while j>i:
 
-			elif begin < end
+                                        loop_j=0
+                                
+                                        #Testa se a palavra em left vem antes da palavra em pivÃ´
+                                        
+                                        if array[i]<pivo:
+                                                        i=i+1
+                                        else:
+                                                        while loop_j==0:
+                                                                        #Atribui a variavÃ©l right a palavra em array[j]
+                                                                        
+                                                                        #Testa se a palavra no pivÃ´ vem antes da palavra em right
+                                                                        if pivo<array[j]:
+                                                                                        j=j-1
+                                                                        else:
+                                                                                        #Troca as palavras no array nas posiÃ§Ãµes i e j
+                                                                                        array[i],array[j] = array[j],array[i]
+                                                                                     
+                                                                                        i+1
+                                                                                        j-1
 
-					#Atribui a variavél num o valor que será retornado em particiona
-					num=self.particiona(array, begin, end)
-
-					#Usa a variavél num para determinar um novo fim e começo para a função quicksort
-
-					self.quicksort(array, begin, num)
-					self.quicksort(array, num+1, end)
-
-					
-					return array
+                                                                                        #Encerra o Loop
+                                                                                        loop_j=loop_j+1
+                        if i>j:
+                                        #Troca as palavras no array nas posiÃ§Ãµes meio e j
+                                        array[begin],array[j]=array[j],array[begin]
+                                        
+                                        print array[j]
+                                        return j
+                        return j                                 
 
 
 
 
+        def QuickSort(self, array, begin, end):
+                        
+                        if 1>=end:
+                                        return array
+
+                        elif begin < end :
+
+                                        #Atribui a variavÃ©l num o valor que serÃ¡ retornado em particiona
+                                        num=self.particiona(array, begin, end)
+                                        #Usa a variavÃ©l num para determinar um novo fim e comeÃ§o para a funÃ§Ã£o quicksort
+                                        self.QuickSort(array, begin, num)
+                                        self.QuickSort(array, num+1, end)
+                                        
+                                        return array
+
+
+
+
+
+
+				
