5907a2a0854874e12b7dad159798cb46e3ed26f2
Victor Rogerio <38105628+VictoRinor@users.noreply.github.com>
2018-04-20 15:17:45 -0300

    "Compilando" e não ordenando
    Eu n entendo bem o porquê de não estar ordenando, mesmo o codigo chegando na situação h=1. Alterei pra usar a função  this_word_comes_first_than_that e como já te disse, por causa de um erro da função remover_acentos, não funciona

--- ShellSort.py
+++ ShellSort.py
@@ -1,34 +1,38 @@
+#This Python file uses the following encoding: utf-8 
 import utilities 
 
 class ShellSort():
-	
-	def sort_array(self, array):
-			
-		size=len(array)
-		h=1
-		
-		#Faz com que a variavél H receba o maior valor possível
-		for h in range(1,size,(h*3)+1):
-			
-		while h>0:
-			i=0
-			j=h
-			
-			#Percorre a array nas posições i e j, na distância h até o final do vetor
-			while j<=h:
-				#Testa para ver se a palavra no array[i] vem antes do array[j]
-				if self.__this_word_comes_first_than_that(array[i], array[j]):
-					
-					#Troca as palavras da array
-					array[i],array[j]=array[j],array[i]
+  
+                def sort_array(self, array):
+                                size=len(array)-1
+                                h=1
+                                #Faz com que a variavÃ©l H receba o maior valor possÃ­vel
+                                while h<size:
+                                        h=(h*3)+1
 
-					#Movimenta as variaveis i e j
-					j=+1
-					i=+1
-			#Atualiza o valor de h
-			h=(h-1)/3			
-
-		return array
-	
-		
-	
+                                while h>=1:
+                                                
+                                                print h
+                                                print '\n'
+                                                i=0
+                                                j=h
+                                                print'entrou'
+                                                #Percorre a array nas posiÃ§Ãµes i e j, na distÃ¢ncia h atÃ© o final do vetor
+                                                while j<size and h<>0:
+                                                                #Testa para ver se a palavra no array[j] vem antes do array[i]
+                                                                if utilities.this_word_comes_first_than_that(array[i],array[j])
+                                                                                
+                                                                                #Troca as palavras da array
+                                                                                array[i],array[j]=array[j],array[i]
+                            
+                                                                #Movimenta as variaveis i e j
+                                                                j=j+h
+                                                                i=i+h
+                                                                print j
+                                                #Atualiza o valor de h             
+                                                h=(h-1)/3
+                                                
+                                                            
+                                                    
+                                return array
+                
